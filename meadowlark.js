79d40ce67a2c3711b0fd13a1f92c89bed62ecf5d
AdobyY <yarinko.b@gmail.com>
2023-10-21 21:23:22 +0300

    added fortune module

--- meadowlark.js
+++ meadowlark.js
@@ -1,15 +1,8 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+const fortune = require('./lib/fortune')
 
 const app = express()
-
-const fortunes = [
-    'Не бійся невідомого',
-    'Тебе чекає сюрприз',
-    'Подивись на небо',
-    'Перейди річку',
-    'Попий кваску'
-]
 
 // Settings for the Handlebars representation engine
 app.engine('handlebars', expressHandlebars.engine({
@@ -22,8 +15,7 @@ const PORT = process.env.PORT || 3000
 app.get('/', (req, res) => {res.render('home')})
 
 app.get('/about', (req, res) => {
-    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-    res.render('about', {fortune: randomFortune})
+    res.render('about', {fortune: fortune.getFortune()})
 })
 
 app.use(express.static(__dirname + '/public'))
