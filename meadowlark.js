8508e4de111ff9f64063bbf5b6a6b42f985472be
AdobyY <yarinko.b@gmail.com>
2023-10-22 10:58:23 +0300

    added handlers

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
-const fortune = require('./lib/fortune')
+const handlers = require('./lib/handlers')
 
 const app = express()
 
@@ -12,26 +12,17 @@ app.set('view engine', 'handlebars')
 
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {res.render('home')})
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-    res.render('about', {fortune: fortune.getFortune()})
-})
+app.get('/about', handlers.about)
 
 app.use(express.static(__dirname + '/public'))
 
 // User page 404
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
+app.use(handlers.notFound)
 
 // User page 500
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError)
 
 app.listen(PORT, () => {
     console.log(`Running Express on PORT:${PORT}`)
