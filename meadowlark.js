9c30899fcf337f6451059329dceabea4ff049194
AdobyY <yarinko.b@gmail.com>
2023-10-26 00:19:55 +0300

    added weather handlebars

--- meadowlark.js
+++ meadowlark.js
@@ -7,6 +7,13 @@ const app = express()
 // Settings for the Handlebars representation engine
 app.engine('handlebars', expressHandlebars.engine({
     defaultLayout: 'main',
+    helpers:{
+        section: function (name, options) {
+            if(!this._sections) this._sections = {}
+            this.__sections[name] = options.fn(this)
+            return null
+        }
+    }
 }))
 app.set('view engine', 'handlebars')
 
@@ -15,6 +22,15 @@ const PORT = process.env.PORT || 3000
 app.get('/', handlers.home)
 
 app.get('/about', handlers.about)
+
+app.get('/headers', (req, res) => {
+    res.type('text/plain')
+    const headers = Object.entries(req.headers)
+        .map(([key, value]) => `${key}: ${value}`)
+        res.send(headers.join('/n'))
+})
+
+
 
 app.use(express.static(__dirname + '/public'))
 
